1c4c909228ed0ce893f055dfd5ce684dd7ec4106
Kyle Harding <kylemharding@gmail.com>
2018-11-18 11:51:43 -0500

    do not generate ssh key

--- start.sh
+++ start.sh
@@ -4,21 +4,16 @@
 # eg. TZ=America/Toronto
 sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && sudo echo $TZ > /etc/timezone
 
-# generate ssh key if one does not exist	
-if [ ! -f "${HOME}/.ssh/id_rsa" ]	
-then	
-	ssh-keygen -q -N '' -f "${HOME}/.ssh/id_rsa"	
-fi
+# set permissions on ssh dir
+[ ! -d "${HOME}/.ssh" ] && mkdir "${HOME}/.ssh"
+chown -R dind:dind "${HOME}/.ssh"
+chmod -R 700 "${HOME}/.ssh"
 
 # if a github username was provided and authorized_keys does not exist
 if [ ! -f "${HOME}/.ssh/authorized_keys" ] && [ -n "${GITHUB_USER}" ]
 then
 	curl -fsSL "https://github.com/${GITHUB_USER}.keys" > "${HOME}/.ssh/authorized_keys"
 fi
-
-# set permissions on ssh dir
-chown -R dind:dind "${HOME}/.ssh"
-chmod -R 700 "${HOME}/.ssh"
 
 # start sshd service in background
 sudo /usr/sbin/sshd -p 22 -D &
