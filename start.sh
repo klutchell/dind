42d034acaa55a05375f46711f2c823593b9dbfee
Kyle Harding <kharding@marchnetworks.com>
2018-11-19 14:31:00 -0500

    change the user create command; skip some steps in the start script

--- start.sh
+++ start.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 
-# set timezone with TZ
-# eg. TZ=America/Toronto
-sudo ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime
-echo "${TZ}" | sudo tee /etc/timezone
+# set timezone if one was provided
+[ -n "${TZ}" ] && sudo ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime && echo "${TZ}" | sudo tee /etc/timezone
 
-# set permissions on ssh dir
-[ ! -d "${HOME}/.ssh" ] && mkdir "${HOME}/.ssh"
-chown -R dind:dind "${HOME}/.ssh"
-chmod -R 700 "${HOME}/.ssh"
+# start docker daemon in the background
+sudo /usr/bin/dockerd ${DIND_OPTS} &
 
-# if a github username was provided and authorized_keys does not exist
-if [ ! -f "${HOME}/.ssh/authorized_keys" ] && [ -n "${GITHUB_USER}" ]
-then
-	curl -fsSL "https://github.com/${GITHUB_USER}.keys" > "${HOME}/.ssh/authorized_keys"
-fi
-
-# start sshd service in background
-sudo /usr/sbin/sshd -p 22 -D &
-
-# start docker daemon in the foreground
-sudo /usr/bin/dockerd ${DIND_OPTS}
+# start sshd service in foreground
+sudo /usr/sbin/sshd -p 22 -D
